96f2ff3641509d42aaf8bbc4de61bd1302c9172d
Shaun Alberts <shaunalberts@gmail.com>
2018-07-08 19:26:25 +0200

    Use localTimestamp as unique identifier for spot forecast

--- app/Sesh/Msw/MswClient.php
+++ app/Sesh/Msw/MswClient.php
@@ -296,7 +296,7 @@ class MswClient
                                 $model = (new MswForecast())->updateOrCreate(
                                     [
                                         'msw_spot_id' => $mswSpot->getId(),
-                                        'timestamp' => $forecast->timestamp,
+                                        'localTimestamp' => $forecast->localTimestamp,
                                     ],
                                     [
                                         'msw_spot_id' => $mswSpot->getId(),
